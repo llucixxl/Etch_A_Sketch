cc373db9c840b4f4515bcbc9ce5c4324c3b8c891
llucixxl <138773138+llucixxl@users.noreply.github.com>
2024-07-09 19:22:52 +0300

    Add reference to grid container and create grid cell variable

--- JS/Js_file.js
+++ JS/Js_file.js
@@ -1,1 +1,12 @@
 // Initial file for the initial commit
+
+// References to container and gridCell creation
+
+const gridContainer = document.querySelector("#grid-container");
+const gridCell = document.createElement("div");
+
+gridCell.setAttribute(
+  "style",
+  "height: 30px; width: 6.25%; border: 1px solid red;"
+);
+gridContainer.appendChild(gridCell);
