8f185df5d5d24cb2cd6b490b5bc97cfffe68cedc
llucixxl <138773138+llucixxl@users.noreply.github.com>
2024-07-09 20:39:56 +0300

    Add function to generate grid board

--- JS/Js_file.js
+++ JS/Js_file.js
@@ -2,11 +2,25 @@
 
 // References to container and gridCell creation
 
-const gridContainer = document.querySelector("#grid-container");
+const gridContainer = document.querySelectorAll("#grid-container");
+const arrayOfGridContainers = Array.from(gridContainer);
 const gridCell = document.createElement("div");
 
-const styledGridCell = gridCell.setAttribute(
+gridCell.setAttribute(
   "style",
   "height: 30px; width: 6.25%; border: 1px solid red;"
 );
-gridContainer.appendChild(gridCell);
+
+// Function to auto-generate the grid board
+
+(function () {
+  let gridNumber = 16;
+
+  for (let i = 0; i < gridNumber; ++i) {
+    arrayOfGridContainers.forEach((container) => {
+      container.appendChild(gridCell.cloneNode(true));
+    });
+  }
+})();
+
+// End of the auto-generate grid board function
