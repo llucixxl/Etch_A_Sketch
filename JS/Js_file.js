a6fda8cf099c4df4f959484361f0deb0a8a4a5d7
llucixxl <138773138+llucixxl@users.noreply.github.com>
2024-07-10 21:16:16 +0300

    Change the j for loop condition

--- JS/Js_file.js
+++ JS/Js_file.js
@@ -6,13 +6,16 @@ const wrapGridContainers = document.querySelector("#wrap-grid-containers");
 
 // Generate grid cells
 
-let gridCellsCount = 100 / 15;
+let gridCellsCount = 100 / 4;
 let newGridCount = 100 / `${gridCellsCount}`;
 
 for (let i = 0; i < newGridCount; i++) {
   const cellBoardContainer = document.createElement("div");
   cellBoardContainer.setAttribute("id", "#wrap-grid-cell");
-  cellBoardContainer.setAttribute("style", "display: flex;");
+  cellBoardContainer.setAttribute(
+    "style",
+    "display: flex; flex-wrap: wrap; flex-grow: 1;"
+  );
 
   for (let j = 0; j < newGridCount; j++) {
     const gridCell = document.createElement("div");
